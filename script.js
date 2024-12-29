c9f1f308dca534b8a0bab68425a37f6f2bd2e08f
Edwin <77319863+Edwin-KT@users.noreply.github.com>
2024-12-29 21:45:16 +0200

    add display logic made the current value a string that can have the max lenght of 9 contain the number that should be displayed and display it at every press of a number button

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const calculatorDisplay = document.querySelector(".display");
+
+const numberButtons = document.querySelectorAll(".number");
+
 function add(firstNumber, secondNumber) {
   return firstNumber + secondNumber;
 }
@@ -31,6 +35,23 @@ function operate() {
   }
 }
 
+function populateDisplay() {
+  calculatorDisplay.textContent = currentValue;
+}
+
+function updateCurrentValue(element) {
+  if (currentValue === "0") currentValue = element.textContent;
+  else if (currentValue.length < 9) currentValue += element.textContent;
+}
+
 let firstOperand = null,
   operator = null,
-  secondOperand = null;
+  secondOperand = null,
+  currentValue = "0";
+
+numberButtons.forEach((element) => {
+  element.addEventListener("click", () => {
+    updateCurrentValue(element);
+    populateDisplay();
+  });
+});
