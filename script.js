1a4a8744703b1df03912a49f9706a21e32386a42
Edwin <77319863+Edwin-KT@users.noreply.github.com>
2025-01-03 21:54:35 +0200

    add calculator functionality made the number, operator, equal and decimal buttons work as intended

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const calculatorDisplay = document.querySelector(".display");
 
+const decimalButton = document.querySelector(".decimal-button");
+const acButton = document.querySelector(".ac-button");
+const equalButton = document.querySelector(".equal-button");
 const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
 
 function add(firstNumber, secondNumber) {
   return firstNumber + secondNumber;
@@ -19,18 +23,20 @@ function divide(firstNumber, secondNumber) {
 }
 
 function operate() {
+  let number1 = parseFloat(firstOperand);
+  let number2 = parseFloat(secondOperand);
   switch (operator) {
     case "+":
-      return add(firstOperand, secondOperand);
+      currentValue = add(number1, number2);
       break;
     case "-":
-      return subtract(firstOperand, secondOperand);
+      currentValue = subtract(number1, number2);
       break;
     case "*":
-      return multiply(firstOperand, secondOperand);
+      currentValue = multiply(number1, number2);
       break;
     case "/":
-      return divide(firstOperand, secondOperand);
+      currentValue = divide(number1, number2);
       break;
   }
 }
@@ -39,19 +45,70 @@ function populateDisplay() {
   calculatorDisplay.textContent = currentValue;
 }
 
+function addDecimal() {
+  if (currentValue.length < 8 && decimal === false) {
+    currentValue += ".";
+    decimal = true;
+  }
+}
+
 function updateCurrentValue(element) {
   if (currentValue === "0") currentValue = element.textContent;
   else if (currentValue.length < 9) currentValue += element.textContent;
 }
 
+function updateOperands() {
+  if (operator != null) evaluate();
+  firstOperand = currentValue;
+  currentValue = "0";
+}
+
+function resetValues() {
+  firstOperand = null;
+  operator = null;
+  secondOperand = null;
+  decimal = false;
+  currentValue = "0";
+}
+
+function evaluate() {
+  secondOperand = currentValue;
+  operate();
+  populateDisplay();
+  firstOperand = currentValue;
+  operator = null;
+  secondOperand = null;
+}
+
 let firstOperand = null,
   operator = null,
   secondOperand = null,
+  decimal = false,
   currentValue = "0";
+
+decimalButton.addEventListener("click", () => {
+  addDecimal();
+});
+
+acButton.addEventListener("click", () => {
+  resetValues();
+  populateDisplay();
+});
+
+equalButton.addEventListener("click", () => {
+  evaluate();
+});
 
 numberButtons.forEach((element) => {
   element.addEventListener("click", () => {
     updateCurrentValue(element);
     populateDisplay();
+  });
+});
+
+operatorButtons.forEach((element) => {
+  element.addEventListener("click", () => {
+    updateOperands();
+    operator = element.textContent;
   });
 });
